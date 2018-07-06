3b0d2f15520b9828a81f5904db5be7a12479c902
Asya Vozvishaeva <30462700+axy7@users.noreply.github.com>
2018-07-06 22:30:32 +0300

    Update site.js

--- template/js/site.js
+++ template/js/site.js
@@ -1,38 +1,37 @@
 $(function() {
-	// popup
-	$('.js-order-tour').click(function(){
-		$('.popup._order').fadeIn();
-	});	
-    $('.popup').click(function() {
-        event = event || window.event;
-        if ($(event.target).closest(".popup__wrapper").length==0) {
-            $('.popup').fadeOut();
-        }
-    }); 
-	$('.popup__close').click(function(){
-		$('.popup').fadeOut();
-	});
-    $(window).keydown(function(e){
-        if(e.keyCode === 27) {
-             $('.popup').fadeOut();
-        }
-    });
+// popup
+$('.js-order-tour').click(function(){
+$('.popup._order').fadeIn();
+});	
+$('.popup').click(function() {
+event = event || window.event;
+if ($(event.target).closest(".popup__wrapper").length==0) {
+$('.popup').fadeOut();
+}
+}); 
+$('.popup__close').click(function(){
+$('.popup').fadeOut();
+});
+$(window).keydown(function(e){
+if(e.keyCode === 27) {
+$('.popup').fadeOut();
+}
+});
 
-    // slider
-    if ($('.slider__item').length > 1) {
-        $('.slider__wrapper').slick({
-            arrows: false,
-            dots: true,
-            respondTo: 'window',
-            fade: true,
-            speed: 800,
-            //autoplay: true,
-            autoplaySpeed: 5000
-        });
-    }
+// slider
+if ($('.slider__item').length > 1) {
+$('.slider__wrapper').slick({
+arrows: false,
+dots: true,
+respondTo: 'window',
+fade: true,
+speed: 800,
+//autoplay: true,
+});
+}
 
-    // other
-    $(".input__text, .input__textarea").textPlaceholder();
-    $(".inputmask").inputmask("+7 (999) 999 99 99", { "clearIncomplete": true });
-    $(".fb").attr('rel','group').fancybox({padding : 0, helpers:{overlay: {locked: false}}});
+// other
+$(".input__text, .input__textarea").textPlaceholder();
+$(".inputmask").inputmask("+7 (999) 999 99 99", { "clearIncomplete": true });
+$(".fb").attr('rel','group').fancybox({padding : 0, helpers:{overlay: {locked: false}}});
 });
